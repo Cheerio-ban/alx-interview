9712956734dcddf1c5c1ac7e31e60d4e909a9cd7
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-18 09:45:50 +0100

    Worked on checker error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
-import sys
-"""ALX SE Backend Module."""
+
+"""
+Handles logs from the file 0-generator.py
+"""
+
+import fileinput
 
 
-status_stats = {}
-total_size = 0
-counter = 0
-allow = [200, 301, 400, 401, 403, 404, 405, 500]
+def print_output(passed_dict, file_size):
+    """Handles printing the output in the required format."""
+    print("File size: {}".format(file_size))
+    passed_dict = dict(sorted(passed_dict.items()))
 
-def print_stats():
-    """Print the stats."""
-    print('File size: {}'.format(total_size))
-    for status in sorted(status_stats.keys()):
-        print('{}: {}'.format(status, status_stats.get(status)))
+    for key, value in passed_dict.items():
+        print("{}: {}".format(key, value))
 
 
-try:
-    for line in sys.stdin:
-        line = line.strip('\n').split()
-        if len(line) != 9:
-            continue
-        if len(line) > 2:
-            counter += 1
-        if int(line[-2]) not in allow:
-            continue
-        status_stats[line[-2]] = 1 + status_stats.get(line[-2], 0)
-        total_size += int(line[-1])
-        if counter == 10:
-            print_stats()
-            counter = 0
-finally:
-    print_stats()
+def get_log():
+    """Handles getting information from the log and is the main program"""
+    file_size = 0
+    status_code = {}
+    try:
+        counter = 0
+        for lines in fileinput.input():
+            try:
+                lines = lines.rstrip("\n")
+                new_list = lines.split()
+
+                if len(new_list) != 9:
+                    continue
+
+                if not new_list[-1].isdigit() or not new_list[-2].isdigit():
+                    continue
+
+                file_size += int(new_list[-1])
+                status_code[int(new_list[-2])] = status_code.get(
+                                                int(new_list[-2]), 0) + 1
+
+                counter += 1
+
+                if counter % 10 == 0:
+                    print_output(status_code, file_size)
+
+            except Exception:
+                pass
+
+
+    except KeyboardInterrupt:
+        print_output(status_code, file_size)
+        # sys.exit(1)
+
+
+if __name__ == '__main__':
+    get_log()
