eb60d202988fe351899ae324dcd834e020fb91c1
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-07-21 00:19:04 +0100

    Replaced with previous code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,48 @@
 #!/usr/bin/python3
-
 """
-Handles logs from the file 0-generator.py
+Reads lines from stdin and prints status codes
+and number of times status code occured after every 10 lines
 """
-
-import fileinput
-
-
-def print_output(passed_dict, file_size):
-    """Handles printing the output in the required format."""
-    print("File size: {}".format(file_size))
-    passed_dict = dict(sorted(passed_dict.items()))
-
-    for key, value in passed_dict.items():
-        print("{}: {}".format(key, value))
-
-
-def get_log():
-    """Handles getting information from the log and is the main program"""
-    file_size = 0
-    status_code = {}
-    try:
-        counter = 0
-        for lines in fileinput.input():
-            try:
-                lines = lines.rstrip("\n")
-                new_list = lines.split()
-
-                if len(new_list) != 9:
-                    continue
-
-                if not new_list[-1].isdigit() or not new_list[-2].isdigit():
-                    continue
-
-                file_size += int(new_list[-1])
-                status_code[int(new_list[-2])] = status_code.get(
-                                                int(new_list[-2]), 0) + 1
-
-                counter += 1
-
-                if counter % 10 == 0:
-                    print_output(status_code, file_size)
-
-            except Exception:
-                pass
-
-
-    except KeyboardInterrupt:
-        print_output(status_code, file_size)
-        # sys.exit(1)
-
 
 if __name__ == '__main__':
-    get_log()
+    import fileinput
+    import sys
+
+    def print_dict(status, size):
+        """Prints a dictionary in sorted order"""
+        print("File size: {}".format(size))
+        status = dict(sorted(status.items()))
+        for elem in status:
+            print("{}: {}".format(elem, status[elem]))
+
+    try:
+        i = 0
+        file_size = 0
+        status_code = {}
+        new_list = [200, 301, 400, 401, 403, 404, 405, 500]
+        for line in fileinput.input():
+            if i % 10 == 0 and i != 0:
+                print_dict(status_code, file_size)
+            line = line.split()
+            if len(line) < 2:
+                continue
+            tmp = line[-2]
+            if ord(tmp[0]) not in list(range(48, 57)):
+                raise
+                continue
+            code = line[-2]
+            size = line[-1]
+            if not code:
+                continue
+            if int(code) not in new_list:
+                continue
+            if code not in status_code:
+                status_code[code] = 1
+            else:
+                status_code[code] += 1
+            file_size += int(size)
+            i += 1
+        print_dict(status_code, file_size)
+    except (IndexError, KeyboardInterrupt):
+        print_dict(status_code, file_size)
+        sys.exit(1)
